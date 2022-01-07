d83b610e87f5a0cf2d9cf614105d5c332f2c96b5
Ivan Kozhokar <dmb14suka@gmail.com>
2022-01-06 18:02:49 -0800

    added fetch api to weather app

---
+++ app.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', ()=> {
+    //variables
+    let long;
+    let lat;
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            //proxy to avoid cors error
+            const proxy = 'https://cors-anywhere.herokuapp.com/'; 
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=7878c94a53b2c53bc0aeea51d25677f6`;
+            //after you get info from api you can do smth with this data/convert it in json
+            fetch(api)
+            .then(response =>{
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                //set Dom elements from the API
+                temperatureDegree.textContent = data.main.temp;
+                locationTimezone.textContent = data.name;
+                temperatureDescription.textContent = data.weather[0].description;
+            });
+        });
+    }
+});
